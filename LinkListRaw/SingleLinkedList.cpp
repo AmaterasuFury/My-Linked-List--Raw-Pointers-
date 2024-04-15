cb30db1228c1dea4a31a9503b0f6e182679e45c3
AmaterasuFury <ibestovu4@gmail.com>
2024-04-15 13:45:57 -0400

    Implement "Add" func

--- LinkListRaw/SingleLinkedList.cpp
+++ LinkListRaw/SingleLinkedList.cpp
@@ -2,25 +2,68 @@
 
 
 
-List::List()
-{
-    Node * Node_Ptr = new Node;
-}
-
+//List::List(){}
 List::~List()
 {
     if(FirstElement->Next == nullptr)
     {
         delete FirstElement;
+        FirstElement = nullptr;
     }
     else
     {
         // make a logic to delete all of the elements; ??? or not?
     }
+}
+
+
+
+void List::Add(int InValue)
+{
+    bool Added = false;
+    Node * TempPtr = FirstElement;
+
+    if(Size == 0)
+    {
+        FirstElement = new Node;
+        FirstElement->value = InValue;
+        return;
+    }
+    else
+    {
+        while (TempPtr->Next != nullptr)
+        {
+            TempPtr = TempPtr->Next;
+        }
+        TempPtr->value = InValue;
+    }
+    if(TempPtr != nullptr)
+    {
+        TempPtr = nullptr;
+    }
+    Size++;
+}
+
+void List::RemoveOnIndex(int InIndex)
+{
+    Node * TempPtr = FirstElement;
+
+    for (int i = 0; i == InIndex; ++i)
+    {
+        
+    }
     
 }
 
-void List::Add(int Value)
+void List::RemoveFirstElement()
 {
-    Node NodePtr;
+    Node * TempPtr = FirstElement;
+
+    if(TempPtr != nullptr)
+    {
+        TempPtr = nullptr;
+    }
+    Size--;
 }
+
+
--- LinkListRaw/SingleLinkedList.h
+++ LinkListRaw/SingleLinkedList.h
@@ -3,21 +3,26 @@
 
 struct List
 {
-    List();
-    ~List();
+private:
     struct Node;
-    
+    //List();
+    ~List();
     int Size = 0;
     Node * FirstElement = nullptr;
-    void Add(int Value);
-    void Remove (int Index);
-    void RemoveValue(int Value);
-    int * Get (int Index);
+
+public:
+    void Add(int InValue);
+    void RemoveOnIndex (int InIndex);
+    void RemoveFirstElement();
+    void RemoveLastElement();
+    void RemoveValue(int InValue);
+    int * Get (int InIndex);
+
     
     
     struct Node
     {
         int value;
-        Node* Next;
+        Node* Next = nullptr;
     };
 };
